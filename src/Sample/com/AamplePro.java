929b0ada6d4ca414c80e197819747499775e173c
UNIFY <UNIFY@192.168.1.245>
2020-01-13 12:11:22 +0530

    added description

--- src/Sample/com/AamplePro.java
+++ src/Sample/com/AamplePro.java
@@ -5,6 +5,10 @@ public class AamplePro {
 	public static void main(String[]args) {
 		
 		System.out.println("Sample project");
+		
+		System.out.println("Sample project");
+		
+		System.out.println("Sample project");
 	}
 
 }
